0c31409b76f432c9b617c47cae3da86c441f0bc3
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-06 20:55:44 -0800

    wrote User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create(
+  [
+    {
+      firstname: "John",
+      lastname: "Doe",
+      email: "johndoe@example.com",
+      username: "johnd123",
+      password: "P@s$w0rd1"
+    },
+    {
+      firstname: "Sally",
+      lastname: "Jefferson",
+      email: "sallyjefferson@example.com",
+      username: "sallyj@example.com",
+      password: "P@s$w0rd2"
+    },
+    {
+      firstname: "Bob",
+      lastname: "McBurger",
+      email: "bobbymac@example.com",
+      username: "bobbym@example.com",
+      password: "P@s$w0rd3"
+    }
+  ]
+)
