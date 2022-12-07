03e7f6480775f3dc0642d3fc7b1f5668ebef9d98
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-06 20:57:23 -0800

    coded out temp user model so I could seed data to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  attr_accessor :firstname, :lastname, :email, :username, :password
+  validates :firstname, :lastname, :email, :username, :password, presence: true
 end
