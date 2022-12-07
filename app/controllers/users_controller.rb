08721aca900b2b526c027f19c01fa24fba00ead6
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-06 22:46:12 -0800

    index() sends all data from users table, create() will add a record to users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    # render template: "users/index"
+    render json: { data: @users }
+  end
+
+  def create
+    user = User.new( firstname: params[:firstname], lastname: params[:lastname], email: params[:email], username: params[:username], password: params[:password])
+    user.save
+  end
 end
